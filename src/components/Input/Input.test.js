46fd2cc9a663a8dc3f554bf1af5865ae9999e476
Sergi Petit <sergi_hak_7@hotmail.com>
2021-10-23 15:35:30 +0200

    Added test for Input component

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,7 +1,24 @@
-import Input from "./Input";
+const { render, screen } = require("@testing-library/react");
+const { default: userEvent } = require("@testing-library/user-event");
+const { default: Input } = require("./Input.js");
 
 describe("When an input component", () => {
   describe("Given a text", () => {
-    test("Then it should");
+    test("Then it should", () => {
+      render(
+        <Input
+          text={""}
+          type={"text"}
+          label={"a"}
+          id={"a"}
+          onChangeAction={() => {}}
+        />
+      );
+
+      const inputEl = screen.getByRole("textbox");
+      userEvent.type(inputEl, "test-text");
+
+      expect(inputEl).toHaveValue("test-text");
+    });
   });
 });
